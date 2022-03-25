7dfdd52dd7781ed28534c81933f9bdc8e63d617e
Lunkubuntu <l.le.mi.lunk15@gmail.com>
2022-03-25 14:23:19 +0100

    Ahora sí, arreglao

---
+++ Scenes/Caballero.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+const moveSpeed = 60
+const maxSpeed = 80
+const jumpHeight = -250
+const up = Vector2(0, -1)
+const gravity = 15
+
+onready var sprite = $Sprite
+onready var animationPlayer = $AnimationPlayer
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y += gravity
+	var friction = false
+	
+	if Input.is_action_pressed("ui_right"):
+		sprite.flip_h = false
+		animationPlayer.play("Walk")
+		motion.x = max(motion.x - moveSpeed, maxSpeed)
+	elif Input.is_action_pressed("ui_left"):
+		sprite.flip_h = true
+		animationPlayer.play("Walk")
+		motion.x = max(motion.x - moveSpeed, -maxSpeed)
+	else:
+		# Se reproduce la animación de estar parado y se para el movimiento
+		animationPlayer.play("Idle")
+		motion.x = 0
+		
+	if is_on_floor():
+		if Input.is_action_pressed("ui_accept"):
+			motion.y = jumpHeight
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.5)
+	else:
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.01)
+			
+	motion = move_and_slide(motion, up)
